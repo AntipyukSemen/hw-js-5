c7b697e08625a268d5a4ad9a2c174bc377cda11a
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-03-25 15:47:31 +0200

    fix 12

--- js/strings-12.js
+++ js/strings-12.js
@@ -1,3 +1,13 @@
-const substring = 'substring length';
-const substringSlice = substring.slice(3, 16);
-console.log(substringSlice);
+let stringData;
+let substringLength;
+stringData = 'hello world';
+substringLength = 6;
+const substring = stringData.slice(0, substringLength);
+console.log(substring);
+
+
+// const message = 'javaScript is fun';
+// const lastIndex = message.length - 1;
+// console.log(lastIndex);
+// const lastLetter = message[lastIndex];
+// console.log(lastLetter);
